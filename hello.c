3a5a95af014bba36031eda77352344ab4357b3f2
primordialsoupe <mkennedy30@huskers.unl.edu>
2021-08-27 23:24:02 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Miguel Kennedy
+ * Date: 2021/08/27
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Miguel Kennedy\n") ;
+
+     return 0;
+ }
